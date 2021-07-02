7b6deef8e2f1a15c9559c38f554ca34a793d03a2
John Kennedy <john_kenn@mac.com>
2021-07-01 18:17:29 -0700

    Initial Commit

---
+++ Assembler6502/Assembler6502App.swift
@@ -0,0 +1,17 @@
+//
+//  Assembler6502App.swift
+//  Assembler6502
+//
+//  Created by John Kennedy on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Assembler6502App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assembler6502/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assembler6502
+//
+//  Created by John Kennedy on 7/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
